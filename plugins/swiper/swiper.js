b418a9195dfa62a6ec1f1f524e283d0126e62cf4
SparkGB <spark991905@gmail.com>
2019-02-18 16:35:07 +0800

    Create Swiper plugin.

---
+++ plugins/swiper/swiper.js
@@ -0,0 +1,67 @@
+'user strict';
+
+var carousel = new Swiper('.hero-section .swiper-container', {
+  loop: false,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+var TabSwiper = function (options) {
+  var init = function (opts) {
+    var lastTabIndex = 0;
+    var currentTabIndex = opts.startAtIndex || 0;
+
+    document.querySelector(`.tab-header .swiper-slide[data-index="${currentTabIndex}"]`).classList.add('active')
+
+    var tabHeader = new Swiper('.tab-header .swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 0,
+      allowTouchMove: false,
+      freeMode: true,
+      slideToClickedSlide: true,
+      breakpoints: {
+        576: {
+          allowTouchMove: true,
+          spaceBetween: 0,
+          slideToClickedSlide: true,
+        },
+      },
+      on: {
+        click: function () {
+          var index = this.clickedIndex
+          var $currentTab = document.querySelector(`.tab-header .swiper-slide[data-index="${index}"]`)
+
+          currentTabIndex = index
+
+          if (lastTabIndex !== currentTabIndex) {
+            document.querySelector(`.tab-header .swiper-slide[data-index="${lastTabIndex}"]`).classList.remove('active')
+            $currentTab.classList.add('active')
+            lastTabIndex = currentTabIndex
+          }
+
+          tabBody.slideTo(index, 500, false)
+        }
+      }
+    })
+
+    var tabBody = new Swiper('.tab-body .swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 0,
+      allowTouchMove: false,
+    })
+  }
+
+  init.call(this, options);
+}
+
+let tabSwiper = new TabSwiper({
+  startAtIndex: 0
+})
