582b39c50f49e2cac91c16f4d4504834ccb14306
SparkGB <spark991905@gmail.com>
2019-02-19 14:22:03 +0800

    Create CI template.

--- plugins/swiper/swiper.js
+++ plugins/swiper/swiper.js
@@ -1,67 +1,80 @@
 'user strict';
 
-var carousel = new Swiper('.hero-section .swiper-container', {
-  loop: false,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  spaceBetween: 0,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
+(function () {
+  var carousel = new Swiper('.hero-section .swiper-container', {
+    loop: false,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
 
-var TabSwiper = function (options) {
-  var init = function (opts) {
-    var lastTabIndex = 0;
-    var currentTabIndex = opts.startAtIndex || 0;
+  var TabSwiper = function (options) {
+    var init = function (opts) {
+      var lastTabIndex = 0;
+      var currentTabIndex = opts.startAtIndex || 0;
 
-    document.querySelector(`.tab-header .swiper-slide[data-index="${currentTabIndex}"]`).classList.add('active')
+      document.querySelector(`.tab-header .swiper-slide[data-index="${currentTabIndex}"]`).classList.add('active')
 
-    var tabHeader = new Swiper('.tab-header .swiper-container', {
-      slidesPerView: 'auto',
-      spaceBetween: 0,
-      allowTouchMove: false,
-      freeMode: true,
-      slideToClickedSlide: true,
-      breakpoints: {
-        576: {
-          allowTouchMove: true,
-          spaceBetween: 0,
-          slideToClickedSlide: true,
+      var tabHeader = new Swiper('.tab-header .swiper-container', {
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+        allowTouchMove: false,
+        freeMode: true,
+        slideToClickedSlide: true,
+        breakpoints: {
+          576: {
+            allowTouchMove: true,
+            spaceBetween: 0,
+            slideToClickedSlide: true,
+          },
         },
-      },
-      on: {
-        click: function () {
-          var index = this.clickedIndex
-          var $currentTab = document.querySelector(`.tab-header .swiper-slide[data-index="${index}"]`)
+        on: {
+          click: function () {
+            var index = this.clickedIndex
+            var $currentTab = document.querySelector(`.tab-header .swiper-slide[data-index="${index}"]`)
 
-          currentTabIndex = index
+            currentTabIndex = index
 
-          if (lastTabIndex !== currentTabIndex) {
-            document.querySelector(`.tab-header .swiper-slide[data-index="${lastTabIndex}"]`).classList.remove('active')
-            $currentTab.classList.add('active')
-            lastTabIndex = currentTabIndex
+            if (lastTabIndex !== currentTabIndex) {
+              document.querySelector(`.tab-header .swiper-slide[data-index="${lastTabIndex}"]`).classList.remove('active')
+              $currentTab.classList.add('active')
+              lastTabIndex = currentTabIndex
+            }
+
+            tabBody.slideTo(index, 500, false)
           }
-
-          tabBody.slideTo(index, 500, false)
         }
-      }
-    })
+      })
 
-    var tabBody = new Swiper('.tab-body .swiper-container', {
-      slidesPerView: 'auto',
-      spaceBetween: 0,
-      allowTouchMove: false,
-    })
+      var tabBody = new Swiper('.tab-body .swiper-container', {
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+        allowTouchMove: false,
+      })
+    }
+
+    init.call(this, options);
   }
 
-  init.call(this, options);
-}
+  let tabSwiper = new TabSwiper({
+    startAtIndex: 0,
+    headerOptions: {
 
-let tabSwiper = new TabSwiper({
-  startAtIndex: 0
-})
+    },
+    bodyOptions: {
+
+    }
+  })
+})()
+
+// TODO:
+// 暴露出tabHeader & tabBody 的 options
+// default index 包成 function
+// 再更細拆分職責
